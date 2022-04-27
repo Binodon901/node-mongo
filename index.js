e94d57207fb948b9211ede6ac30a6aa99692b014
Binodon901 <s.h.binodon.ict@gmail.com>
2022-04-27 14:38:25 +0600

    init

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send("Thank yoy for calling me")
 })
 
+app.get('/fruit',(req,res)=>{
+    res.send({fruit:'Banana',quqntity:'100',price:100})
+})
+
 app.listen(3000,()=>{
     console.log('listen port 3000');
 })
